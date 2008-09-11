d80f0c9720073432da1cc6b41f3a4bcabc9d224c
david_calavera <david_calavera@dev.java.net>
2008-09-11 17:01:44 +0000

    rubyMetrics first commit. It includes Rcov coverage support.

---
+++ src/main/java/hudson/plugins/rubyMetrics/rcov/RcovProjectAction.java
@@ -0,0 +1,79 @@
+package hudson.plugins.rubyMetrics.rcov;
+
+import java.io.IOException;
+
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.Actionable;
+import hudson.model.ProminentProjectAction;
+import hudson.model.Result;
+
+public class RcovProjectAction extends Actionable implements ProminentProjectAction {
+
+	private final AbstractProject<?, ?> project;
+	
+	public RcovProjectAction(AbstractProject<?, ?> project) {
+		this.project = project;
+	}
+	
+	public AbstractProject<?, ?> getProject() {
+        return project;
+    }
+	
+	public String getDisplayName() {
+		return "Rcov report";
+	}
+
+	public String getIconFileName() {
+		return "graph.gif";
+	}
+
+	public String getUrlName() {
+		return "rcov";
+	}
+
+    public String getSearchUrl() {
+        return getUrlName();
+    }
+	
+	public RcovBuildAction getLastResult() {
+		for (AbstractBuild<?, ?> b = project.getLastStableBuild(); b != null; b = b.getPreviousNotFailedBuild()) {
+	        if (b.getResult() == Result.FAILURE)
+	            continue;
+	        RcovBuildAction r = b.getAction(RcovBuildAction.class);
+	        if (r != null)
+	            return r;
+	    }
+	    return null;
+	}
+	
+	public Integer getLastResultBuild() {
+		for (AbstractBuild<?, ?> b = project.getLastStableBuild(); b != null; b = b.getPreviousNotFailedBuild()) {
+            if (b.getResult() == Result.FAILURE)
+                continue;
+            RcovBuildAction r = b.getAction(RcovBuildAction.class);
+            if (r != null)
+                return b.getNumber();
+        }
+        return null;
+	}
+	
+	public void doGraph(StaplerRequest req, StaplerResponse rsp) throws IOException {
+        if (getLastResult() != null) {
+            getLastResult().doGraph(req, rsp);
+        }
+    }
+
+    public void doIndex(StaplerRequest req, StaplerResponse rsp) throws IOException {
+        Integer buildNumber = getLastResultBuild();
+        if (buildNumber == null) {
+            rsp.sendRedirect2("nodata");
+        } else {
+            rsp.sendRedirect2("../" + buildNumber + "/rcov");
+        }
+    }
+
+}
---
+++ src/main/java/hudson/plugins/rubyMetrics/rcov/RcovPublisher.java
@@ -0,0 +1,135 @@
+package hudson.plugins.rubyMetrics.rcov;
+
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.Util;
+import hudson.model.AbstractProject;
+import hudson.model.Action;
+import hudson.model.Build;
+import hudson.model.BuildListener;
+import hudson.model.Descriptor;
+import hudson.model.Project;
+import hudson.model.Result;
+import hudson.plugins.rubyMetrics.rcov.model.RcovResult;
+import hudson.tasks.Publisher;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Rcov {@link Publisher}
+ * 
+ * @author David Calavera
+ *
+ */
+public class RcovPublisher extends Publisher {
+	
+	private final String reportDir;
+	
+	@DataBoundConstructor
+	public RcovPublisher(String reportDir) {
+		this.reportDir = reportDir;				
+	}
+	
+	public String getReportDir() {
+		return reportDir;
+	}
+	
+	/**
+     * {@inheritDoc}
+     */
+    public boolean perform(Build<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+    	if (!Result.SUCCESS.equals(build.getResult())) {
+    		listener.getLogger().println("Build wasn't successful, skipping rcov coverage report");
+    		return true;
+    	}
+    	listener.getLogger().println("Publishing Rcov report...");
+    	
+        final Project<?, ?> project = build.getParent();        
+        final FilePath workspace = project.getModuleRoot();        
+    	
+        boolean copied = moveReportsToBuildRootDir(workspace, build, listener);
+        if (!copied) {
+        	return fail(build, listener, "Rcov report directory wasn't found using the pattern '" + reportDir + "'.");
+        }
+        
+        final RcovFilenameFilter indexFilter = new RcovFilenameFilter();
+        
+        File[] coverageFiles = build.getRootDir().listFiles(indexFilter);
+        if (!(coverageFiles != null && coverageFiles.length > 0)) {
+        	return fail(build, listener, "Rcov report index file wasn't found");
+        }
+        
+        RcovParser parser = new RcovParser(build.getRootDir());
+        RcovResult results = parser.parse(coverageFiles[0]);
+    	
+        RcovBuildAction action = new RcovBuildAction(build, results);        
+        build.getActions().add(action);
+        
+    	return true;
+    }
+    
+    private boolean moveReportsToBuildRootDir(FilePath workspace, Build<?, ?> build, BuildListener listener) throws InterruptedException {    	
+        try {        	
+        	FilePath coverageDir = workspace.child(reportDir);
+            
+        	if (!coverageDir.exists()) {
+        		listener.getLogger().println("file not found: " + coverageDir);
+        		return false;
+        	}
+            
+            int copied = coverageDir.copyRecursiveTo("**/*", new FilePath(build.getRootDir()));
+            
+            return true;
+        } catch (IOException e) {
+            Util.displayIOException(e, listener);
+            e.printStackTrace(listener.fatalError("Unable to find coverage results"));
+            build.setResult(Result.FAILURE);
+        }
+        return false;
+    }
+    
+    private boolean fail(Build<?, ?> build, BuildListener listener, String message) {
+    	listener.getLogger().println(message);
+        build.setResult(Result.FAILURE);
+    	return true;
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    public Action getProjectAction(final AbstractProject<?, ?> project) {
+        return new RcovProjectAction(project);
+    }
+	
+	/**
+     * Descriptor should be singleton.
+     */
+    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+
+    public static final class DescriptorImpl extends Descriptor<Publisher> {
+
+		protected DescriptorImpl() {
+			super(RcovPublisher.class);			
+		}
+
+		@Override
+		public String getDisplayName() {
+			return "Rcov coverage publisher";
+		}
+    	
+    }
+
+	public Descriptor<Publisher> getDescriptor() {
+		return DESCRIPTOR;
+	}
+	
+	private static class RcovFilenameFilter implements FilenameFilter {		
+        public boolean accept(File dir, String name) {            
+            return name.equalsIgnoreCase("index.html");
+        }
+    }
+	
+}
