e5b8944c28f800b0d5a3b863823c7dfb0afd7665
david_calavera <david_calavera@dev.java.net>
2009-12-30 13:03:53 +0000

    flog support improvements. Pretty paths to files, and changed workspace by module root

--- src/main/java/hudson/plugins/rubyMetrics/flog/FlogParser.java
+++ src/main/java/hudson/plugins/rubyMetrics/flog/FlogParser.java
@@ -2,9 +2,14 @@ package hudson.plugins.rubyMetrics.flog;
 
 import hudson.plugins.rubyMetrics.flog.model.FlogFileResults;
 import hudson.plugins.rubyMetrics.flog.model.FlogMethodResults;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jregex.Matcher;
 import jregex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.plexus.util.StringOutputStream;
 
 public class FlogParser {
@@ -12,11 +17,11 @@ public class FlogParser {
 	private final static Pattern operatorRegex = new Pattern("\\s*({score}\\d+\\.\\d+):\\s({operator}.*)$");
 	private final static Pattern methodRegex = new Pattern("\\s*({score}\\d+\\.\\d+):\\s+({method}[A-Za-z:]+(?:#|::).*)");
 	
-	public FlogFileResults parse(StringOutputStream results) {
-		return parse(results.toString());
+	public FlogFileResults parse(String filePath, StringOutputStream results) {
+		return parse(filePath, results.toString());
 	}
 	
-	public FlogFileResults parse(String results) {
+	public FlogFileResults parse(String filePath, String results) {
 		String[] resultsSplit = results.split("\n\n");
 		if (resultsSplit == null || resultsSplit.length == 0) {
 			return null;
@@ -28,7 +33,7 @@ public class FlogParser {
 		FlogFileResults flogResults = new FlogFileResults(total, average);
 		for (int index = 1; index < resultsSplit.length; index++) {
 			for (String line : resultsSplit[index].split("\n")) {
-				addFlogResults(flogResults, line);
+				addFlogResults(filePath, flogResults, line);
 			}
 		}
 		
@@ -41,10 +46,11 @@ public class FlogParser {
 		return matcher.group("score");
 	}
 	
-	private void addFlogResults(FlogFileResults flogResults, String line) {
+	private void addFlogResults(String filePath, FlogFileResults flogResults, String line) {
 		Matcher matcher = methodRegex.matcher(line);
 		if (matcher.matches()) {
-			FlogMethodResults methodResults = new FlogMethodResults(matcher.group("method"), matcher.group("score"));
+			String methodName = prettifyMethodPath(filePath, matcher.group("method"));
+			FlogMethodResults methodResults = new FlogMethodResults(methodName, matcher.group("score"));
 			flogResults.addMethodResult(methodResults);
 		} else {
 			matcher = operatorRegex.matcher(line);
@@ -52,5 +58,21 @@ public class FlogParser {
 				flogResults.addOperatorResult(matcher.group("operator"), matcher.group("score"));
 			}
 		}
+	}
+	
+	private String prettifyMethodPath(String filePath, String methodName) {
+		if (!methodName.contains(filePath)) {
+			return methodName;
+		}
+		String[] split = methodName.split("\\s+");
+		
+		Collection<String> line = new ArrayList<String>();
+		for (String s : split) {
+			if (s.contains(filePath)) {
+				s = s.substring(s.indexOf(filePath));
+			}
+			line.add(s);
+		}
+		return StringUtils.join(line, " ");
 	}
 }
