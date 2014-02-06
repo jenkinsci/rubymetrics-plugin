6a60eba6e71595ca64511966fa9b5708b9bceafe
Mike Dillon <mike@embody.org>
2014-02-06 13:51:16 -0800

    Display any exception message from loadSourceCode

--- src/main/java/hudson/plugins/rubyMetrics/rcov/model/RcovFileDetail.java
+++ src/main/java/hudson/plugins/rubyMetrics/rcov/model/RcovFileDetail.java
@@ -7,12 +7,16 @@ import hudson.plugins.rubyMetrics.rcov.RcovParser;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.logging.Logger;
+import static java.util.logging.Level.SEVERE;
 
 import org.htmlparser.util.ParserException;
 
 public class RcovFileDetail implements ModelObject, Serializable  {
 
     private static final long serialVersionUID = -3496008428347123532L;
+
+    private static final Logger LOGGER = Logger.getLogger(RcovFileDetail.class.getName());
 
     private final AbstractBuild<?, ?> owner;
     private final RcovFileResult result;
@@ -38,10 +42,17 @@ public class RcovFileDetail implements ModelObject, Serializable  {
         return "Rcov report for: " + result.getName();
     }
 
-    public String loadSourceCode() throws IOException, ParserException {
-        RcovParser parser = new RcovParser(owner.getRootDir());
-        return parser.parseSource(getResult().getLinkPath());
+    public String loadSourceCode() {
+        try {
+            RcovParser parser = new RcovParser(owner.getRootDir());
+            return parser.parseSource(getResult().getLinkPath());
+        } catch (IOException e) {
+            LOGGER.log(SEVERE, Messages.RcovFileDetail_ParseError(result.getName()), e);
+            return Messages.RcovFileDetail_ParseErrorHtml(e.getMessage());
+        } catch (ParserException e) {
+            LOGGER.log(SEVERE, Messages.RcovFileDetail_ParseError(result.getName()), e);
+            return Messages.RcovFileDetail_ParseErrorHtml(e.getMessage());
+        }
     }
-
 
 }
